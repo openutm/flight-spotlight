c12e575ba087b43a532f3d7d97c644210df0eb8e
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-03-23 12:50:25 +0000

    Added flight declarations

--- routes/auth.js
+++ routes/auth.js
@@ -15,15 +15,24 @@ const jwtAuthz = require('express-jwt-authz');
 const bbox = require("@turf/bbox");
 const intersect = require("@turf/intersect");
 const flip = require('@turf/flip');
+const axios = require('axios');
 require("dotenv").config();
 var URL = require('url').URL;
 let geojsonhint = require("@mapbox/geojsonhint");
-const { check, validationResult } = require('express-validator');
-const { get } = require("https");
-const { head } = require("request");
+const {
+  check,
+  validationResult
+} = require('express-validator');
+const {
+  get
+} = require("https");
+const {
+  head
+} = require("request");
 const redis = require("redis");
 const redis_client = redis.createClient();
 const async = require("async");
+
 
 redis_client.on("error", function (error) {
   console.error(error);
@@ -44,11 +53,17 @@ const checkJwt = jwt({
 
 
 router.get("/", (req, res) => {
-  res.render("home", { title: "Home" });
+  res.render("home", {
+    title: "Home"
+  });
 });
 
 router.get("/noticeboard", secured(), (req, response, next) => {
-  const { _raw, _json, ...userProfile } = req.user;
+  const {
+    _raw,
+    _json,
+    ...userProfile
+  } = req.user;
   const bing_key = process.env.BING_KEY || 'get-yours-at-https://www.bingmapsportal.com/';
   const mapbox_key = process.env.MAPBOX_KEY || 'thisIsMyAccessToken';
   const mapbox_id = process.env.MAPBOX_ID || 'this_is_my_mapbox_map_id';
@@ -65,7 +80,11 @@ router.get("/noticeboard", secured(), (req, response, next) => {
 });
 
 router.get("/spotlight", secured(), (req, response, next) => {
-  const { _raw, _json, ...userProfile } = req.user;
+  const {
+    _raw,
+    _json,
+    ...userProfile
+  } = req.user;
   const bing_key = process.env.BING_KEY || 'get-yours-at-https://www.bingmapsportal.com/';
   const mapbox_key = process.env.MAPBOX_KEY || 'thisIsMyAccessToken';
   const mapbox_id = process.env.MAPBOX_ID || 'this_is_my_mapbox_map_id';
@@ -83,19 +102,38 @@ router.get("/spotlight", secured(), (req, response, next) => {
 
 
 router.post("/set_air_traffic", checkJwt, jwtAuthz(['spotlight.write.air_traffic']), [
-  check('lat_dd').isFloat({ min: -180.00, max: 180.00 }),
-  check('lon_dd').isFloat({ min: -180.00, max: 180.00 }),
-  check('altitude_mm').isFloat({ min: 0.00 }),
-  check('time_stamp').isInt({ gt: 1, allow_leading_zeroes: false }),
-  check('traffic_source').isInt({ gt: 1, lt: 10 }),
-  check('source_type').isInt({ gt: -1, lt: 10 }),
-  check('icao_address').isString()],
+    check('lat_dd').isFloat({
+      min: -180.00,
+      max: 180.00
+    }),
+    check('lon_dd').isFloat({
+      min: -180.00,
+      max: 180.00
+    }),
+    check('altitude_mm').isFloat({
+      min: 0.00
+    }),
+    check('time_stamp').isInt({
+      gt: 1,
+      allow_leading_zeroes: false
+    }),
+    check('traffic_source').isInt({
+      gt: 1,
+      lt: 10
+    }),
+    check('source_type').isInt({
+      gt: -1,
+      lt: 10
+    }),
+    check('icao_address').isString()
+  ],
   (req, response, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return response.status(422).json({ errors: errors.array() });
-    }
-    else {
+      return response.status(422).json({
+        errors: errors.array()
+      });
+    } else {
 
       const req_body = req.body;
       const lat_dd = req_body.lat_dd;
@@ -105,7 +143,12 @@ router.post("/set_air_traffic", checkJwt, jwtAuthz(['spotlight.write.air_traffic
       const source_type = req_body.source_type;
       const icao_address = req_body.icao_address;
       try {
-        client.set('observation', icao_address, [lon_dd, lat_dd, altitude_mm], { 'source_type': source_type, 'traffic_source': traffic_source }, { expire: 300 });
+        client.set('observation', icao_address, [lon_dd, lat_dd, altitude_mm], {
+          'source_type': source_type,
+          'traffic_source': traffic_source
+        }, {
+          expire: 300
+        });
       } catch (err) {
         console.log("Error " + err);
       }
@@ -129,9 +172,10 @@ router.post("/set_geo_fence", checkJwt, jwtAuthz(['spotlight.write.geo_fence']),
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return response.status(422).json({ errors: errors.array() });
-  }
-  else {
+    return response.status(422).json({
+      errors: errors.array()
+    });
+  } else {
     const req_body = req.body;
     const geo_fence = req_body.geo_fence;
     const geo_fence_properties = JSON.parse(req_body.properties);
@@ -140,36 +184,41 @@ router.post("/set_geo_fence", checkJwt, jwtAuthz(['spotlight.write.geo_fence']),
     let upper_limit = geo_fence_properties['upper_limit'];
     let lower_limit = geo_fence_properties['lower_limit'];
 
-    client.set('geo_fence', 'geo_fence', JSON.parse(geo_fence), { 'upper_limit': upper_limit, 'lower_limit': lower_limit });
+    client.set('geo_fence', 'geo_fence', JSON.parse(geo_fence), {
+      'upper_limit': upper_limit,
+      'lower_limit': lower_limit
+    });
 
-    response.send({ "message": "OK" });
+    response.send({
+      "message": "OK"
+    });
   }
 });
 
-router.post("/set_flight_declaration", checkJwt, jwtAuthz(['spotlight.write.flight_declaration']), check('flight_declaration').custom(submitted_flight_declaration => {
-  let options = {};
-  submitted_flight_declaration = JSON.parse(submitted_flight_declaration);
+// router.post("/set_flight_declaration", checkJwt, jwtAuthz(['spotlight.write.flight_declaration']), check('flight_declaration').custom(submitted_flight_declaration => {
+//   let options = {};
+//   submitted_flight_declaration = JSON.parse(submitted_flight_declaration);
 
-  let errors = geojsonhint.hint(submitted_flight_declaration['flight_declaration']['parts'], options);
-  if (errors.length > 0) {
-    throw new Error('Invalid Flight Declaration supplied.');
-  } else {
-    return true;
-  }
-}), (req, response, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return response.status(422).json({ errors: errors.array() });
-  }
-  else {
-    const req_body = req.body;
-    const f_d = JSON.parse(req_body.flight_declaration);
-    const flight_id = f_d.flight_id;
-    redis_client.hset('fd', flight_id, JSON.stringify(f_d));
-    redis_client.expire(flight_id, 3600);
-    response.send('OK');
-  }
-});
+//   let errors = geojsonhint.hint(submitted_flight_declaration['flight_declaration']['parts'], options);
+//   if (errors.length > 0) {
+//     throw new Error('Invalid Flight Declaration supplied.');
+//   } else {
+//     return true;
+//   }
+// }), (req, response, next) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return response.status(422).json({ errors: errors.array() });
+//   }
+//   else {
+//     const req_body = req.body;
+//     const f_d = JSON.parse(req_body.flight_declaration);
+//     const flight_id = f_d.flight_id;
+//     redis_client.hset('fd', flight_id, JSON.stringify(f_d));
+//     redis_client.expire(flight_id, 3600);
+//     response.send('OK');
+//   }
+// });
 
 
 // router.post("/get_registry_data",secured(), [
@@ -213,6 +262,93 @@ router.get("/get_flight_declarations", secured(), (req, response, next) => {
 
 });
 
+router.get("/retrieve_flight_declarations", secured(), (req, response, next) => {
+  const base_url = process.env.BLENDER_BASE_URL || 'http://local.test:8000';
+  
+  redis_key = 'passport_token';
+  let start_date = req.query['start_date'];
+  let end_date = req.query['end_date'];
+  async.map([redis_key], function (r_key, done) {
+
+    redis_client.get(r_key, function (err, results) {
+      if (err || results == null) {
+        let post_data = {
+          "client_id": process.env.PASSPORT_CLIENT_ID,
+          "client_secret": process.env.PASSPORT_CLIENT_SECRET,
+          "grant_type": "client_credentials",
+          "scope": process.env.PASSPORT_BLENDER_SCOPE,
+          "audience": process.env.PASSPORT_BLENDER_AUDIENCE
+        };
+        axios.request({
+          url: "/oauth/token/",
+          method: "post",
+          header: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          baseURL: process.env.PASSPORT_URL,
+          data: qs.stringify(post_data)
+        }).then(passport_response => {
+
+          if (passport_response.status == 200) {
+            let a_token = passport_response.data;
+            let access_token = JSON.stringify(a_token);
+            redis_client.set(r_key, access_token);
+            redis_client.expire(r_key, 3500);
+
+            req.flash("success", "Thanks for the message! I‘ll be in touch :)");
+            res.redirect("/");
+            return done(null, a_token);
+          } else {
+
+            return done(null, {
+              "error": "Error in Passport Query, response not 200"
+            });
+          }
+        }).catch(axios_err => {
+
+          return done(null, {
+            "error": "Error in Passport Query, error in paramters supplied, check Client ID and / or secret"
+          });
+        });
+
+      } else {
+        let a_token = JSON.parse(results);
+        return done(null, a_token);
+      }
+
+    });
+  }, function (error, redis_output) {
+
+    try {
+      var passport_token = redis_output[0]['access_token'];
+    } catch {
+      var passport_token = {
+        "error": "Error in parsing token, check redis client call"
+      }
+    }
+
+    let url = base_url + '/flight_declaration?start_date='+ start_date+ '&end_date=' +end_date;
+    axios.get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': "Bearer " + passport_token
+        }
+      })
+      .then(function (blender_response) {
+        
+        if (blender_response.status == 200) {
+
+          response.send(blender_response.data);
+        } else {
+
+          // console.log(error);
+          response.send(blender_response.data);
+        }
+      });
+  });
+
+
+});
 
 router.post("/set_aoi", secured(), check('geo_json').custom(submitted_aoi => {
   let options = {};
@@ -226,16 +362,21 @@ router.post("/set_aoi", secured(), check('geo_json').custom(submitted_aoi => {
 }), (req, response, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return response.status(422).json({ errors: errors.array() });
-  }
-  else {
+    return response.status(422).json({
+      errors: errors.array()
+    });
+  } else {
     const aoi = JSON.parse(req.body.geo_json);
     const email = req.body.email;
     const aoi_bbox = bbox(aoi['features'][0]);
     var io = req.app.get('socketio');
     let geo_fence_query = client.intersectsQuery('geo_fence').object(aoi['features'][0]);
     geo_fence_query.execute().then(results => {
-      io.sockets.in(email).emit("message", { 'type': 'message', "alert_type": "aoi_geo_fence", "results": results });
+      io.sockets.in(email).emit("message", {
+        'type': 'message',
+        "alert_type": "aoi_geo_fence",
+        "results": results
+      });
       return results;
     }).then(geo_fence => {
       // Setup a Geofence for the results 
@@ -249,7 +390,11 @@ router.post("/set_aoi", secured(), check('geo_json').custom(submitted_aoi => {
             console.error("something went wrong! " + err);
           } else {
             let status = geo_fence_results.id + ": " + geo_fence_results.detect + " geo fence area";
-            io.sockets.in(email).emit("message", { 'type': 'message', "alert_type": "geo_fence_crossed", "results": status });
+            io.sockets.in(email).emit("message", {
+              'type': 'message',
+              "alert_type": "geo_fence_crossed",
+              "results": status
+            });
           }
         });
         geo_fence_stream.onClose(() => {
@@ -266,7 +411,11 @@ router.post("/set_aoi", secured(), check('geo_json').custom(submitted_aoi => {
       if (err) {
         console.error("something went wrong! " + err);
       } else {
-        io.sockets.in(email).emit("message", { 'type': 'message', "alert_type": "aoi", "results": results });
+        io.sockets.in(email).emit("message", {
+          'type': 'message',
+          "alert_type": "aoi",
+          "results": results
+        });
       }
     });
 
@@ -274,7 +423,9 @@ router.post("/set_aoi", secured(), check('geo_json').custom(submitted_aoi => {
       console.log("AOI geofence was closed");
     });
 
-    response.send({ 'msg': "Scanning flights in AOI and Geofences" });
+    response.send({
+      'msg': "Scanning flights in AOI and Geofences"
+    });
 
   };
 });
@@ -282,7 +433,11 @@ router.post("/set_aoi", secured(), check('geo_json').custom(submitted_aoi => {
 
 /* GET user profile. */
 router.get('/user', secured(), function (req, res, next) {
-  const { _raw, _json, ...userProfile } = req.user;
+  const {
+    _raw,
+    _json,
+    ...userProfile
+  } = req.user;
   res.render('user', {
     userProfile: JSON.stringify(userProfile, null, 2),
     title: 'Profile page'
@@ -300,10 +455,16 @@ router.get('/login', passport.authenticate('oauth2', {
 
 router.get('/callback', function (req, res, next) {
   passport.authenticate('oauth2', function (err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { return res.redirect('/login'); }
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect('/login');
+    }
     req.logIn(user, function (err) {
-      if (err) { return next(err); }
+      if (err) {
+        return next(err);
+      }
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
       res.redirect(returnTo || '/spotlight');
@@ -320,9 +481,9 @@ router.get("/logout", (req, res) => {
 
   if (port !== undefined && port !== 80 && port !== 443) {
     returnTo =
-      process.env.NODE_ENV === "production"
-        ? `${returnTo}/`
-        : `${returnTo}:${port}/`;
+      process.env.NODE_ENV === "production" ?
+      `${returnTo}/` :
+      `${returnTo}:${port}/`;
   }
 
   const logoutURL = new URL(
