fbb9bfe3eed924899371dd0a8f9ff85c9dec069e
hrishiballal <hrishikeshballal@yahoo.com>
2022-07-12 12:45:36 +0100

    Removed Timeine and added vis.js calls

---
+++ static/js/vis-timeline/timeline-render.js
@@ -0,0 +1,42 @@
+   function render_timeline(all_flight_declarations){
+    if (all_flight_declarations.length > 0 ) {
+        var container = document.getElementById('visualization');
+        const declarations_len = all_flight_declarations.length;
+        var op_types = { 1: 'VLOS', 2: 'BVLOS' };
+        const dataset_items = []
+        if (declarations_len !== 0) {
+            for (var j1 = 0; j1 < declarations_len; j1++) {
+                var cur_declaration = all_flight_declarations[j1];
+                const start_date_time = new Date(cur_declaration.start_datetime);
+                const end_date_time = new Date(cur_declaration.end_datetime);
+                dataset_items.push({ id: j1, content: cur_declaration.originating_party, start: start_date_time, end: end_date_time })
+
+            }
+            
+            // Create a DataSet (allows two way data-binding)
+            var items = new vis.DataSet(dataset_items);
+
+            // Configuration for the Timeline
+            var options = {"editable":false};
+
+            // Create a Timeline
+            var timeline = new vis.Timeline(container, items, options);
+        }
+    }
+    function move (percentage) {
+        var range = timeline.getWindow();
+        var interval = range.end - range.start;
+
+        timeline.setWindow({
+            start: range.start.valueOf() - interval * percentage,
+            end:   range.end.valueOf()   - interval * percentage
+        });
+    }
+
+    // attach events to the navigation buttons
+    document.getElementById('zoomIn').onclick    = function () { timeline.zoomIn( 0.2); };
+    document.getElementById('zoomOut').onclick   = function () { timeline.zoomOut( 0.2); };
+    document.getElementById('moveLeft').onclick  = function () { move( 0.2); };
+    document.getElementById('moveRight').onclick = function () { move(-0.2); };
+
+}
